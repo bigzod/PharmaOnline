0d2ad2bbbebbcb8d638782c74dee3afc4ba55c9e
nehri97 <doublerico7@gmail.com>
2015-07-27 23:08:48 -0400

    ..
    ...

--- PharmaOnline/settings.py
+++ PharmaOnline/settings.py
@@ -16,8 +16,6 @@ import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
